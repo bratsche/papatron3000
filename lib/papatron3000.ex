49ea63e1785a04430820bc5aa0f104ab4a3fa715
Cody Russell <cody@jhu.edu>
2022-05-20 22:46:44 -0500

    👴🏻

---
+++ lib/papatron3000.ex
@@ -0,0 +1,18 @@
+defmodule Papatron3000 do
+  @moduledoc """
+  Documentation for `Papatron3000`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Papatron3000.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/papatron3000_test.exs
@@ -0,0 +1,8 @@
+defmodule Papatron3000Test do
+  use ExUnit.Case
+  doctest Papatron3000
+
+  test "greets the world" do
+    assert Papatron3000.hello() == :world
+  end
+end
